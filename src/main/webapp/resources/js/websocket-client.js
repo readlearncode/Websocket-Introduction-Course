30bd591cf81a55a66e353a32ebf986dfc332ee1d
Alex Theedom <alex.theedom@gmail.com>
2017-04-03 18:37:21 +0100

    Change endpoint URI in javascript file

--- src/main/webapp/resources/js/websocket-client.js
+++ src/main/webapp/resources/js/websocket-client.js
@@ -2,13 +2,55 @@
  * Created by readlearncode.com on 29/11/2016.
  */
 var wsocket;
-var serviceLocation = "ws://localhost:8080/dukechat/chat";
+var serviceLocation = "ws://localhost:8080/dukechat/chat/";
 var $nickName;
 var $message;
 var $chatWindow;
 var $alert;
 var room = '';
-var user = '';
+
+function onMessageReceived(evt) {
+
+    var msg = JSON.parse(evt.data); // native API
+
+    var $messageLine = $('<tr><td class="received">' + msg.received
+        + '</td><td class="user label label-info">' + msg.sender
+        + '</td><td class="message badge">' + msg.content
+        + '</td></tr>');
+    $chatWindow.append($messageLine);
+}
+
+function sendMessage() {
+    var msg = '{"content":"' + $message.val() + '", "sender":"'
+        + $nickName.val() + '", "received":""}';
+    wsocket.send(msg);
+    $message.val('').focus();
+}
+
+function connectToChatserver() {
+    room = $('#chatroom option:selected').val();
+    nickName = $('#nickname').val();
+    wsocket = new WebSocket(serviceLocation + room);
+    // wsocket = new WebSocket(serviceLocation + "/?sender=" +  encodeURI(nickName));
+    wsocket.onerror = onConnectionError;
+    wsocket.onmessage = onMessageReceived;
+}
+
+function onConnectionError(evt) {
+
+    var msg = evt; // native API
+    var $messageLine = $(evt);
+    $chatWindow.append($messageLine);
+
+}
+
+function leaveRoom() {
+    wsocket.close();
+    $chatWindow.empty();
+    $('.chat-wrapper').hide();
+    $('.chat-signin').show();
+    $nickName.focus();
+}
 
 $(document).ready(function () {
     $nickName = $('#nickname');
@@ -16,18 +58,12 @@ $(document).ready(function () {
     $chatWindow = $('#response');
     $alert = $('#alert');
     $('.chat-wrapper').hide();
+    $nickName.focus();
 
     $('#enterRoom').click(function (evt) {
         evt.preventDefault();
-
-        // Extract room selected from drop-down
-        room = $('#chatroom option:selected').val();
-        user = $('#nickname').val();
-
-        // Connect to chat server
-        connectToChatServer();
-
-        $('.chat-wrapper h2').html("Welcome " + user + "! <br/>You are in the " + room + " chat room.");
+        connectToChatserver();
+        $('.chat-wrapper h2').text('Chat # ' + $nickName.val() + "@" + room);
         $('.chat-signin').hide();
         $('.chat-wrapper').show();
         $message.focus();
@@ -42,52 +78,3 @@ $(document).ready(function () {
     });
 });
 
-function onMessageReceived(evt) {
-    // Parse JSON String to JavaScript Object
-    var msg = JSON.parse(evt.data); // native API
-
-    // Construct HTML snippet and print to screen
-    var $messageLine = constructHTMLSnippet(msg.sender, msg.content, msg.received);
-    $chatWindow.append($messageLine);
-}
-
-function sendMessage() {
-    // Construct message to send to server
-    var msg = '{"content":"' + $message.val() + '", "sender":"' + user + '", "received":"' + '"}';
-    wsocket.send(msg);
-
-    // Put back focus
-    $message.val('').focus();
-}
-
-function connectToChatServer() {
-    // Create websocket connection to server endpoint URI
-    wsocket = new WebSocket(constructURI(serviceLocation, encodeURI(room), user));
-
-    // Set message and error handlers
-    wsocket.onerror = onConnectionError;
-    wsocket.onmessage = onMessageReceived;
-}
-
-function onConnectionError(evt) {
-    // Print error to chat window
-    $alert.append($(evt));
-}
-
-function leaveRoom() {
-    wsocket.close();
-    $chatWindow.empty();
-    $('.chat-wrapper').hide();
-    $('.chat-signin').show();
-}
-
-function constructHTMLSnippet(nickName, content, received) {
-    return $('<tr><td class="received">' + received.substring(0, 8)
-        + '</td><td class="user">' + nickName
-        + '</td><td class="message">' + content
-        + '</td></tr>');
-}
-
-function constructURI(serviceLocation, room, user) {
-    return serviceLocation + "/" + room + "/" + user;
-}
