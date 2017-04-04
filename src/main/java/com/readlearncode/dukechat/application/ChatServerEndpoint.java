c2054ca310446d84c67454d179a3ec82f529f8e2
Alex Theedom <alex.theedom@gmail.com>
2017-04-04 10:35:31 +0100

    Add programmatic endpoints

--- src/main/java/com/readlearncode/dukechat/application/ChatServerEndpoint.java
+++ src/main/java/com/readlearncode/dukechat/application/ChatServerEndpoint.java
@@ -1,20 +1,11 @@
 package com.readlearncode.dukechat.application;
 
 import com.readlearncode.dukechat.domain.Message;
-import com.readlearncode.dukechat.domain.MessageEvent;
 import com.readlearncode.dukechat.domain.Room;
-import com.readlearncode.dukechat.infrastructure.MessageDecoder;
-import com.readlearncode.dukechat.infrastructure.MessageEncoder;
-import com.readlearncode.dukechat.infrastructure.cdi.MessageReceived;
 
 import javax.annotation.PostConstruct;
-import javax.enterprise.event.Event;
-import javax.inject.Inject;
 import javax.websocket.*;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -25,11 +16,12 @@ import static com.readlearncode.dukechat.utils.Messages.WELCOME_MESSAGE;
 import static com.readlearncode.dukechat.utils.Messages.objectify;
 
 /**
- * @author Alex Theedom
+ * Source code github.com/readlearncode
+ *
+ * @author Alex Theedom www.readlearncode.com
  * @version 1.0
  */
-@ServerEndpoint(value = "/chat/{roomName}/{userName}", encoders = MessageEncoder.class, decoders = MessageDecoder.class)
-public class ChatServerEndpoint {
+public class ChatServerEndpoint extends Endpoint {
 
     private final static Logger log = Logger.getLogger(ChatServerEndpoint.class.getSimpleName());
 
@@ -37,56 +29,53 @@ public class ChatServerEndpoint {
 
     private static final String[] roomNames = {"Java EE 7", "Java SE 8", "Websockets", "JSON"};
 
-    @Inject
-    @MessageReceived
-    private Event<MessageEvent> messageReceived;
-
     @PostConstruct
     public void initialise() {
         Arrays.stream(roomNames).forEach(roomName -> rooms.computeIfAbsent(roomName, new Room(roomName)));
     }
 
-    @OnOpen
-    public void onOpen(final Session session, @PathParam("roomName") final String roomName, @PathParam("userName") final String userName,EndpointConfig conf) throws IOException, EncodeException {
+    @Override
+    public void onOpen(Session session, EndpointConfig config) {
+
+        System.out.println("onOpen " + session);
+
+        // Add Message message handler
+        session.addMessageHandler((MessageHandler.Whole<Message>) message ->
+                {
+                    System.out.println("message " + message);
+                    rooms.get(extractRoomFrom(session)).sendMessage(message);
+                }
+        );
 
         // Set session level configurations
+        String roomName = session.getPathParameters().get("roomName");
+        String userName = session.getPathParameters().get("userName");
         session.getUserProperties().putIfAbsent("roomName", roomName);
         session.getUserProperties().putIfAbsent("userName", userName);
+        session.setMaxIdleTimeout(5 * 60 * 1000); // Timeouts after 5 minutes
 
-        // Timeouts after 5 minutes
-        session.setMaxIdleTimeout(5 * 60 * 1000);
+        System.out.println("roomName " + roomName);
+        System.out.println("userName " + userName);
 
-        // Store session in room
+        // Store session
         Room room = rooms.get(roomName);
         room.join(session);
 
         // Send welcome message
-        session.getBasicRemote().sendObject(objectify(WELCOME_MESSAGE));
+        try {
+            session.getBasicRemote().sendObject(objectify(WELCOME_MESSAGE));
+        } catch (IOException | EncodeException e) {
+            log.info("Welcome message not sent!");
+        }
+
     }
 
-    @OnMessage
-    public void onMessage(Session session, Message message) throws IOException, EncodeException {
-        rooms.get(extractRoomFrom(session)).sendMessage(message);
-        messageReceived.fire(new MessageEvent(message, session));
-    }
-
-    @OnMessage
-    public void onBinaryMessage(ByteBuffer message, Session session) {
-        // Not implemented
-    }
-
-    @OnMessage
-    public void onPongMessage(PongMessage message, Session session) {
-        // Not implemented
-    }
-
-    @OnClose
-    public void onClose(Session session, CloseReason reason) throws IOException, EncodeException {
+    // Optional
+    public void onClose(Session session, CloseReason reason) {
         log.info(reason::getReasonPhrase);
         rooms.get(extractRoomFrom(session)).leave(session);
     }
 
-    @OnError
     public void onError(Session session, Throwable error) {
         log.info(error::getMessage);
         // implement error handling
