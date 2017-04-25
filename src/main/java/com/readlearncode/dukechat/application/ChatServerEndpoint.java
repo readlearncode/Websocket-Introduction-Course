320090d4ddfffb290c80493e8764d83a00c66cc1
Alex Theedom <alex.theedom@gmail.com>
2017-04-24 20:57:47 -0700

    Add start state for lesson 4

--- src/main/java/com/readlearncode/dukechat/application/ChatServerEndpoint.java
+++ src/main/java/com/readlearncode/dukechat/application/ChatServerEndpoint.java
@@ -1,15 +1,9 @@
 package com.readlearncode.dukechat.application;
 
-import com.readlearncode.dukechat.domain.Message;
 import com.readlearncode.dukechat.domain.Room;
-import com.readlearncode.dukechat.infrastructure.MessageDecoder;
-import com.readlearncode.dukechat.infrastructure.MessageEncoder;
 
 import javax.annotation.PostConstruct;
-import javax.websocket.*;
-import javax.websocket.server.PathParam;
-import javax.websocket.server.ServerEndpoint;
-import java.nio.ByteBuffer;
+import javax.websocket.Session;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -19,7 +13,6 @@ import java.util.Map;
  * @author Alex Theedom www.readlearncode.com
  * @version 1.0
  */
-@ServerEndpoint(value = "/chat/{roomName}/{userName}", encoders = MessageEncoder.class, decoders = MessageDecoder.class)
 public class ChatServerEndpoint {
 
     private static final Map<String, Room> rooms = Collections.synchronizedMap(new HashMap<String, Room>());
@@ -31,37 +24,7 @@ public class ChatServerEndpoint {
         Arrays.stream(roomNames).forEach(roomName -> rooms.computeIfAbsent(roomName, new Room(roomName)));
     }
 
-    @OnOpen
-    public void onOpen(final Session session,
-                       @PathParam("roomName") final String roomName,
-                       @PathParam("userName") final String userName )  {
-        // Implement open session logic
-    }
 
-    @OnMessage
-    public void onMessage(Session session, Message message)  {
-        // Implement message receive logic
-    }
-
-    @OnMessage
-    public void onBinaryMessage(ByteBuffer message, Session session) {
-        // Not implemented
-    }
-
-    @OnMessage
-    public void onPongMessage(PongMessage message, Session session) {
-        // Not implemented
-    }
-
-    @OnClose
-    public void onClose(Session session, CloseReason reason)  {
-        // Implement close session logic
-    }
-
-    @OnError
-    public void onError(Session session, Throwable error) {
-        // Implement error logic
-    }
 
     /**
      * Extracts the room from the session
