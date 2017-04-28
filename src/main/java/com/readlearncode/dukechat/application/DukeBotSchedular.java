784acad21b553c13ee5e202bae545cf24c6746a3
Alex Theedom <alex.theedom@gmail.com>
2017-04-27 19:04:53 -0700

    Merge remote-tracking branch 'origin/07_03_end' into 07_03_end
    # Conflicts:
    #	src/main/java/com/readlearncode/dukechat/application/DukeBotSchedular.java

---
+++ src/main/java/com/readlearncode/dukechat/application/DukeBotSchedular.java
@@ -0,0 +1,24 @@
+package com.readlearncode.dukechat.application;
+
+
+import com.readlearncode.dukechat.utils.Messages;
+
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+import java.util.logging.Logger;
+
+/**
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Stateless
+class DukeBotSchedular {
+
+    private static final Logger logger = Logger.getLogger(DukeBotSchedular.class.getName());
+
+    @Schedule(minute = "*/20", hour = "*")
+    private void interrupt() {
+        ChatServerEndpoint.getRooms().forEach((s, room) -> room.sendMessage(Messages.objectify("Hello from Duke Bot")));
+    }
+
+}
