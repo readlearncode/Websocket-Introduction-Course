7f4904845cd8f213857819d36293c1b455d8b7d6
Alex Theedom <alex.theedom@gmail.com>
2017-05-01 18:29:10 +0100

    Update wepapp folder and add DukeBotSchedular

---
+++ src/main/java/com/readlearncode/dukechat/application/DukeBotSchedular.java
@@ -0,0 +1,21 @@
+package com.readlearncode.dukechat.application;
+
+
+import com.readlearncode.dukechat.utils.Messages;
+
+import javax.ejb.Schedule;
+import javax.ejb.Stateless;
+
+/**
+ * @author Alex Theedom www.readlearncode.com
+ * @version 1.0
+ */
+@Stateless
+class DukeBotSchedular {
+
+    @Schedule(minute = "*/20", hour = "*")
+    private void interrupt() {
+        ChatServerEndpoint.getRooms().forEach((s, room) -> room.sendMessage(Messages.objectify("Hello from Duke Bot")));
+    }
+
+}
